36e73127ce426ba19012af50c6e1c0da19f04472
Sajmir Doko <sajdoko@gmail.com>
2020-01-18 12:13:20 +0100

    boilerplate

---
+++ includes/class-cf7-multi-step-conditional-addon-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.linkedin.com/in/sajmirdoko/
+ * @since      1.0.0
+ *
+ * @package    Cf7_Multi_Step_Conditional_Addon
+ * @subpackage Cf7_Multi_Step_Conditional_Addon/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Cf7_Multi_Step_Conditional_Addon
+ * @subpackage Cf7_Multi_Step_Conditional_Addon/includes
+ * @author     Sajmir Doko <sajdoko@gmail.com>
+ */
+class Cf7_Multi_Step_Conditional_Addon_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/partials/cf7-multi-step-conditional-addon-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://www.linkedin.com/in/sajmirdoko/
+ * @since      1.0.0
+ *
+ * @package    Cf7_Multi_Step_Conditional_Addon
+ * @subpackage Cf7_Multi_Step_Conditional_Addon/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
